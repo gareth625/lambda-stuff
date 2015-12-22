9f92846d6f40c7b112b47fb51bbff7ec151fca43
gareth625 <gareth.j.rogers@gmail.com>
2015-12-22 21:35:27 +0000

    Initial commit of an cljs-lambda created project with the role modified so that it works :)

---
+++ project.clj
@@ -0,0 +1,25 @@
+(defproject lambda-stuff "0.1.0-SNAPSHOT"
+  :description "FIXME"
+  :url "http://please.FIXME"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.145"]
+                 [org.clojure/core.async "0.2.371"]
+                 [io.nervous/cljs-lambda "0.1.2"]]
+  :plugins [[lein-cljsbuild "1.0.6"]
+            [lein-npm "0.5.0"]
+            [io.nervous/lein-cljs-lambda "0.2.4"]]
+  :node-dependencies [[source-map-support "0.2.8"]]
+  :source-paths ["src"]
+  :cljs-lambda
+  {:defaults {:role "arn:aws:iam::141786013431:role/lambda_role"}
+   :functions
+   [{:name   "work-magic"
+     :invoke lambda-stuff.core/work-magic}]}
+  :cljsbuild
+  {:builds [{:id "lambda-stuff"
+             :source-paths ["src"]
+             :compiler {:output-to "out/lambda_stuff.js"
+                        :output-dir "out"
+                        :target :nodejs
+                        :optimizations :none
+                        :source-map true}}]})
---
+++ src/lambda_stuff/core.cljs
@@ -0,0 +1,9 @@
+(ns lambda-stuff.core
+  (:require [cljs-lambda.util :refer [async-lambda-fn]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+(def ^:export work-magic
+  (async-lambda-fn
+   (fn [{:keys [variety]} context]
+     (go
+       (js/Error (str "Sorry, I don't yet know how to work: '" variety "' magic"))))))
