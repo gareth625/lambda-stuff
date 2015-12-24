6711facb4fee902b723b133821d6cbfbd38816bb
gareth625 <gareth.j.rogers@gmail.com>
2015-12-24 12:16:11 +0000

    Now attempting to put and get items from a DynamoDB. Not very successful so far.

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.2.371"]
-                 [io.nervous/cljs-lambda "0.1.2"]]
+                 [io.nervous/cljs-lambda "0.1.2"]
+                 [io.nervous/eulalie "0.6.4"]
+                 [io.nervous/hildebrand "0.4.3"]
+                 ]
   :plugins [[lein-cljsbuild "1.0.6"]
             [lein-npm "0.5.0"]
             [io.nervous/lein-cljs-lambda "0.2.4"]]
@@ -13,8 +16,10 @@
   :cljs-lambda
   {:defaults {:role "arn:aws:iam::141786013431:role/lambda_role"}
    :functions
-   [{:name   "work-magic"
-     :invoke lambda-stuff.core/work-magic}]}
+   [{:name   "add-exercise"
+     :invoke lambda-stuff.core/add-exercise}
+    {:name   "get-exercise"
+     :invoke lambda-stuff.core/get-exercise}]}
   :cljsbuild
   {:builds [{:id "lambda-stuff"
              :source-paths ["src"]
--- src/lambda_stuff/core.cljs
+++ src/lambda_stuff/core.cljs
@@ -1,9 +1,40 @@
 (ns lambda-stuff.core
-  (:require [cljs-lambda.util :refer [async-lambda-fn]])
-  (:require-macros [cljs.core.async.macros :refer [go]]))
+    (:require [cljs-lambda.util :refer [async-lambda-fn]]
+              [eulalie.creds]
+              [hildebrand.channeled :refer [query!]]
+              [hildebrand.core :refer [put-item!]])
+    (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(def ^:export work-magic
-  (async-lambda-fn
-   (fn [{:keys [variety]} context]
-     (go
-       (js/Error (str "Sorry, I don't yet know how to work: '" variety "' magic"))))))
+(def exercise->type
+    {"barbell-deadlift" :weighted})
+
+(def exercise-type->schema
+    {:weighted #{:exercise :date-hour :weight-kg :reps}})
+
+(defn exercise->schema
+    [exercise]
+    (exercise-type->schema (exercise->type exercise)))
+
+(defn build-item
+    [event schema]
+    (into {} (map (fn [k] (hash-map k (event (name k)))) schema)))
+
+(def ^:export add-exercise
+    (let [table :exercise]
+        (async-lambda-fn
+            (fn [{:keys [exercise date-hour] :as event} context]
+                (go
+                    (let [creds (assoc :region "eu-west-1" (eulalie.creds/env))]
+                         (if (and exercise date-hour)
+                             (put-item! creds table (build-item (exercise->schema exercise) event))
+                             (js/Error (str "Sorry, you must specify an exercise and date-hour (time of exercise to the hour). Was given exercise: '" + exercise + "' and date-hour: '" + date-hour + "'.")))))))))
+
+(def ^:export get-exercise
+    (let [table :exercise]
+         (async-lambda-fn
+             (fn [{:keys [exercise]} context]
+                  (go
+                      (let [creds (assoc :region "eu-west-1" (eulalie.creds/env))]
+                           (if exercise
+                               (js/Error "Wooo") ;; (query! creds table {:exercise [:= exercise]})
+                               (js/Error "No exercise was specified."))))))))
