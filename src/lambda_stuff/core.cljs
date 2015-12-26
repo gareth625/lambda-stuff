8924835d5ac6c36c3f03ec2f3ed501efa49c1b99
gareth625 <gareth.j.rogers@gmail.com>
2015-12-26 21:42:35 +0000

    Bug fix to remove the unnecessary name call. It also includes some attempts to get things to work but that's not going so well.

--- src/lambda_stuff/core.cljs
+++ src/lambda_stuff/core.cljs
@@ -2,7 +2,8 @@
   (:require [cljs-lambda.util :refer [async-lambda-fn]]
             [eulalie.creds]
             [hildebrand.channeled :refer [query!]]
-            [hildebrand.core :refer [put-item!]])
+            [hildebrand.core :refer [put-item!]]
+            [cljs.core.async :refer [take! <!]])
     (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (def exercise->type
@@ -17,7 +18,7 @@
 
 (defn build-item
   [event schema]
-  (into {} (map (fn [k] (hash-map k (event (name k)))) schema)))
+  (into {} (map (fn [k] (hash-map k (event k))) schema)))
 
 (def ^:export add-exercise
   (let [table :exercise]
@@ -26,7 +27,7 @@
         (go
           (let [creds (assoc (eulalie.creds/env) :region "eu-west-1")]
             (if (and exercise date-hour)
-              (put-item! creds table (build-item (exercise->schema exercise) event))
+              (<! (put-item! creds table (build-item (exercise->schema exercise) event)))
               (js/Error (str "Sorry, you must specify an exercise and date-hour (time of exercise to the hour). Was given exercise: '" + exercise + "' and date-hour: '" + date-hour + "'.")))))))))
 
 (def ^:export get-exercise
@@ -35,6 +36,6 @@
       (fn [{:keys [exercise]} context]
         (go
           (let [creds (assoc (eulalie.creds/env) :region "eu-west-1")]
-          (if exercise
-            (query! creds table {:exercise [:= exercise]} {:limit 10})
-            (js/Error "No exercise was specified."))))))))
+            (if exercise
+              (<! (query! creds table {:exercise [:= exercise]} {:limit 10}))
+              (js/Error "No exercise was specified."))))))))
